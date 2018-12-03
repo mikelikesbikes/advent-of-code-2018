2633512ed7324197bccb4689a23cdf89d4e79f17
Mike Busch <mbusch@heroku.com>
2018-12-03 11:02:05 -0500

    day 3 ruby

---
+++ day-3/day_3.rb
@@ -0,0 +1,57 @@
+module FabricAnalyzer
+  def pristine_claims(io)
+    read_claims(io)
+      .yield_self { |claims| [claims, build_claims_map(claims)] }
+      .yield_self do |claims, claims_map|
+        claim = claims.find do |claim|
+          claim_coords(claim).all? { |coord| claims_map[coord] == 1 }
+        end
+        claim.first
+      end
+  end
+
+  def overlap(io)
+    read_claims(io)
+      .yield_self { |claims| build_claims_map(claims) }
+      .count { |_, v| v > 1 }
+  end
+
+  def read_claims(io)
+    io.readlines
+      .map { |line| parse(line) }
+  end
+
+  def build_claims_map(claims)
+    claims.each_with_object(Hash.new(0)) do |claim, claims_map|
+      claim_coords(claim).each do |coord|
+        claims_map[coord] += 1
+      end
+    end
+  end
+
+  def claim_coords(claim)
+    _, (x, y), (width, length) = claim
+    width.times.each_with_object([]) do |i, coords|
+      length.times do |j|
+        coords << [x + i, y + j]
+      end
+    end
+  end
+
+  def parse(line)
+    id, _, start, dimensions = line.split(" ")
+    coord = start.slice(0..-2).split(",").map(&:to_i)
+    width_length = dimensions.split("x").map(&:to_i)
+    [id, coord, width_length]
+  end
+
+  extend self
+end
+
+return unless $PROGRAM_NAME == __FILE__
+
+File.open(File.expand_path("./input.txt")) do |file|
+  puts FabricAnalyzer.overlap(file)
+  file.rewind
+  puts FabricAnalyzer.pristine_claims(file)
+end
---
+++ day-3/day_3_spec.rb
@@ -0,0 +1,20 @@
+require "rspec"
+require_relative "./day_3"
+
+describe FabricAnalyzer do
+  let(:input) do
+    StringIO.new <<~INPUT
+      #1 @ 1,3: 4x4
+      #2 @ 3,1: 4x4
+      #3 @ 5,5: 2x2
+    INPUT
+  end
+
+  it "finds the number of inches of overlap" do
+    expect(FabricAnalyzer.overlap(input)).to eq 4
+  end
+
+  it "finds claims that have no conflicting claims" do
+    expect(FabricAnalyzer.pristine_claims(input)).to eq "#3"
+  end
+end
