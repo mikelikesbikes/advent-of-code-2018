223602c273db80a7bc7b873c6e7ebf5ad211115f
Mike Busch <mbusch@heroku.com>
2018-12-10 02:01:19 -0500

    refactor day 10 to nearly unreadable

--- day-10/day_10.rb
+++ day-10/day_10.rb
@@ -3,30 +3,13 @@ module MessageFinder
     input
       .map { |line| parse_point_of_light(line) }
       .yield_self do |points_of_light|
-        area1 = 2**32 * 2**32
-        area2 = covered_area(points_of_light)
-
-        pol1 = nil
-        pol2 = points_of_light
-
+        next_area = 2**32 * 2**32
         n = 0
-        while area1 > area2
-          pol1 = pol2
-          area1 = area2
-          pol2 = tick(pol1)
-          area2 = covered_area(pol2)
-          n += 1
-        end
+        n += 1 while (current_area = next_area) > (next_area = covered_area(points_of_light_at_tick(points_of_light, n)))
+        n -= 1
 
-        [n - 1, plot_message(pol1)]
+        [n, plot_message(points_of_light_at_tick(points_of_light, n))]
       end
-  end
-
-  def parse_point_of_light(str)
-    _, position_str, velocity_str = str.match(/position=<(.*)> velocity=<(.*)>/).to_a
-    x, y = position_str.split(",").map(&:strip).map(&:to_i)
-    dx, dy = velocity_str.split(",").map(&:strip).map(&:to_i)
-    [[x, y], [dx, dy]]
   end
 
   def covered_area(pol)
@@ -35,25 +18,32 @@ module MessageFinder
   end
 
   def bounds(pol)
-    xs = Range.new(*pol.map { |((x, _), _)| x }.minmax)
-    ys = Range.new(*pol.map { |((_, y), _)| y }.minmax)
+    xs = Range.new(*pol.map(&:first).minmax)
+    ys = Range.new(*pol.map(&:last).minmax)
     [xs, ys]
   end
 
-  def tick(pol)
+  def points_of_light_at_tick(pol, n)
     pol.map do |(x, y), vel|
-      [[x + vel.first, y + vel.last], vel]
+      [x + (n * vel.first), y + (n * vel.last)]
     end
   end
 
   def plot_message(pol)
     x_range, y_range = bounds(pol)
-    pol_hash = Hash[pol]
+    pol_hash = Hash[pol.zip([true]*pol.size)]
     y_range.map do |y|
       x_range.map do |x|
         pol_hash[[x, y]] ? "#" : "."
       end.join
     end.join("\n")
+  end
+
+  def parse_point_of_light(str)
+    _, position_str, velocity_str = str.match(/position=<(.*)> velocity=<(.*)>/).to_a
+    x, y = position_str.split(",").map(&:strip).map(&:to_i)
+    dx, dy = velocity_str.split(",").map(&:strip).map(&:to_i)
+    [[x, y], [dx, dy]]
   end
 
   extend self
