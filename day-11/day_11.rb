0afd82288258c9d94a04cf6da1ea1c4e24e74033
Mike Busch <mbusch@heroku.com>
2018-12-11 09:48:06 -0500

    day 11 refactor to use a class

--- day-11/day_11.rb
+++ day-11/day_11.rb
@@ -1,38 +1,42 @@
-module FuelIndicator
-  def fuel_level(*args)
-    @fuel_level ||= Hash.new do |h, k|
-      (x, y), serial_number = k
-      h[k] = ((((((x + 10) * y) + serial_number) * (x + 10)) % 1000) / 100) - 5
-    end
-    @fuel_level[args]
+class FuelIndicator
+  attr_reader :serial_number
+  def initialize(serial_number)
+    @serial_number = serial_number
   end
 
-  def total_power((x, y), size, serial_number)
+  def fuel_level(x, y, size = 1)
+    @fuel_level ||= Hash.new do |size_hash, size|
+      size_hash[size] = Hash.new do |coord_hash, (x, y)|
+        coord_hash[[x, y]] = ((((((x + 10) * y) + serial_number) * (x + 10)) % 1000) / 100) - 5
+      end
+    end
+    @fuel_level[size][[x, y]]
+  end
+
+  def total_power((x, y), size)
     total = 0
     (0...size).each do |dx|
       (0...size).each do |dy|
-        total += fuel_level([x + dx, y + dy], serial_number)
+        total += fuel_level(x + dx, y + dy)
       end
     end
     total
   end
 
-  def largest_total_power_block(serial_number, size = 3)
+  def largest_total_power_block_for_size(size = 3)
     totals = 1.upto(300 - size).each_with_object({}) do |y, totals|
       1.upto(300 - size).each do |x|
-        totals[[x, y]] = total_power([x, y], size, serial_number)
+        totals[[x, y]] = total_power([x, y], size)
       end
     end
     totals.max_by { |_, v| v }
   end
 
-  def largest_total_power_block_with_size(serial_number)
+  def largest_total_power_block
     (1..50)
-      .map { |size| p [*largest_total_power_block(serial_number, size), size] }
+      .map { |size| p [*largest_total_power_block_for_size(size), size] }
       .max_by { |_, _, size| size }
   end
-
-  extend self
 end
 
 return unless $PROGRAM_NAME == __FILE__
@@ -40,5 +44,5 @@ return unless $PROGRAM_NAME == __FILE__
 filename = ARGV.shift || File.expand_path("input.txt", __dir__)
 input = File.read(filename).strip.to_i
 
-puts FuelIndicator.largest_total_power_block(input)
-puts FuelIndicator.largest_total_power_block_with_size(input)
+puts FuelIndicator.new(input).largest_total_power_block_for_size
+puts FuelIndicator.new(input).largest_total_power_block
--- day-11/day_11_spec.rb
+++ day-11/day_11_spec.rb
@@ -1,23 +1,19 @@
 require_relative "./day_11.rb"
 
 describe FuelIndicator do
-  let(:input) do
-    ""
-  end
-
   it "calculates fuel level of a given cell" do
-    expect(FuelIndicator.fuel_level([3, 5], 8)).to eq 4
-    expect(FuelIndicator.fuel_level([122, 79], 57)).to eq -5
-    expect(FuelIndicator.fuel_level([217, 196], 39)).to eq 0
-    expect(FuelIndicator.fuel_level([101, 153], 71)).to eq 4
+    expect(FuelIndicator.new(8).fuel_level(3, 5)).to eq 4
+    expect(FuelIndicator.new(57).fuel_level(122, 79)).to eq -5
+    expect(FuelIndicator.new(39).fuel_level(217, 196)).to eq 0
+    expect(FuelIndicator.new(71).fuel_level(101, 153)).to eq 4
   end
 
   it "finds the 3x3 square with the largest total power" do
-    expect(FuelIndicator.largest_total_power_block(18).first).to eq [33, 45]
+    expect(FuelIndicator.new(18).largest_total_power_block_for_size(3)).to eq [33, 45]
   end
 
   it "finds the square of any size with the largest total power" do
-    expect(FuelIndicator.largest_total_power_block_with_size(18)).to eq [90,269,16]
-    expect(FuelIndicator.largest_total_power_block_with_size(42)).to eq [232,251,12]
+    expect(FuelIndicator.new(18).largest_total_power_block).to eq [90,269,16]
+    expect(FuelIndicator.new(42).largest_total_power_block).to eq [232,251,12]
   end
 end
